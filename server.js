532b55522a617760c036a2e7a02d6f746c543ec6
Ujjwal Dahal <ujjwaldahal35@gmail.com>
2020-04-14 12:40:41 +0545

    Bootcamp model

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 const PORT = process.env.PORT || 5000;
-
+const connectDB = require('./config/db');
 // load env vars
 dotenv.config({ path: './config/config.env' });
+
+// connect to DB
+connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
@@ -19,6 +23,15 @@ if (process.env.NODE_ENV === 'development') {
 // mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode in port ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode in port ${PORT}`.yellow.bold
+  );
+});
+
+// handle unhandled rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // close server & exit process
+  server.close(() => process.exit(1));
 });
