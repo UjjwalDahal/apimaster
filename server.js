7405d6588fd8bed72a7ca4a250982e4c09706601
Ujjwal Dahal <ujjwaldahal35@gmail.com>
2020-06-05 10:03:51 +0545

    added auth route and controllers with jwt

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const path = require("path");
 const colors = require("colors");
 const fileUpload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 
@@ -16,10 +17,15 @@ connectDB();
 // Route files
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
+const auth = require("./routes/auth");
 
 const app = express();
 
+// body parser
 app.use(express.json());
+
+// cookie parser
+app.use(cookieParser());
 
 // dev logging middleware
 if (process.env.NODE_ENV === "development") {
@@ -36,6 +42,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
 
 // error middleware
 app.use(errorHandler);
