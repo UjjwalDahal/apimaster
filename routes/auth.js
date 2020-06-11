69db3d620cae42328fa6c56c946bef44327a962f
Ujjwal Dahal <ujjwaldahal35@gmail.com>
2020-06-11 14:50:06 +0545

    Added update user email and name

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  updateDetails,
 } = require("../controllers/auth");
 const { protect } = require("../middleware/auth");
 const router = express.Router();
@@ -13,6 +14,7 @@ router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/forgotpassword").post(forgotPassword);
 router.route("/resetpassword/:resettoken").put(resetPassword);
+router.route("/updatedetails").put(protect, updateDetails);
 
 router.route("/me").get(protect, getMe);
 
