ba371145f641ad167a6e9f2d181409fe9542195d
Ujjwal Dahal <ujjwaldahal35@gmail.com>
2020-06-11 12:53:20 +0545

    Reset password token generate and save

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { register, login, getMe } = require("../controllers/auth");
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+} = require("../controllers/auth");
 const { protect } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/forgotpassword").post(forgotPassword);
 
 router.route("/me").get(protect, getMe);
 
