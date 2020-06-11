096fa8fc8a22e8141f3de8798944633b2a0318be
Ujjwal Dahal <ujjwaldahal35@gmail.com>
2020-06-11 13:33:46 +0545

    added reset password route with reset token

--- controllers/auth.js
+++ controllers/auth.js
@@ -83,7 +83,7 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   // Create reset URL
   const resetURL = `${req.protocol}//${req.get(
     "host"
-  )}/api/v1/resetpassword/${resetToken}`;
+  )}/api/v1/auth/resetpassword/${resetToken}`;
 
   const message = `Request made to reset the password. Please make a PUT request to: \n\n ${resetURL}`;
 
@@ -114,6 +114,37 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     success: true,
     data: user,
   });
+});
+
+// @desc      Reset password
+// @route     PUT /api/v1/auth/resetpassword/:resettoken
+// @access    Public
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+  // get hashed token
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.resettoken)
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: {
+      $gt: Date.now(),
+    },
+  });
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid Token", 400));
+  }
+
+  // Set new Password
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
 });
 
 // Get token from model, create cookie and send response
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const {
   login,
   getMe,
   forgotPassword,
+  resetPassword,
 } = require("../controllers/auth");
 const { protect } = require("../middleware/auth");
 const router = express.Router();
@@ -11,6 +12,7 @@ const router = express.Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/forgotpassword").post(forgotPassword);
+router.route("/resetpassword/:resettoken").put(resetPassword);
 
 router.route("/me").get(protect, getMe);
 
